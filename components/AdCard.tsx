116ec6d8ff092eb9003c8fa4686e7a7e51ae120e
Hemanth M <hemanth00405@gmail.com>
2024-12-23 15:11:05 +0530

    feat: integrate lazy loading ad script and fallback image in AdCard component

--- components/AdCard.tsx
+++ components/AdCard.tsx
@@ -1,7 +1,12 @@
-export function AdCard() {
+import Script from 'next/script';
+import Image from 'next/image';
 
+export function AdCard() {
     return (
-        <div id="container-9fa17e65afd06a9c8b61c435fc6b42cb" className="bg-card/40 text-card-foreground p-6 rounded-lg glass-effect backdrop-blur-sm h-full"></div>
+        <div className="bg-card/40 text-card-foreground p-6 rounded-lg glass-effect backdrop-blur-sm h-full">
+            <Script strategy="lazyOnload" src="https://udbaa.com/bnr.php?section=General&pub=623378&format=300x250&ga=g" />
+            <noscript><a href="https://yllix.com/publishers/623378" target="_blank"><Image width={300} height={250} src="//ylx-aff.advertica-cdn.com/pub/300x250.png" className="border-none m-0 p-0 align-baseline" alt="ylliX - Online Advertising Network" /></a></noscript>
+        </div>
     )
 }
 
